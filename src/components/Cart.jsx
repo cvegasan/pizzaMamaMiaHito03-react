5548a6206869f830f3a8d7956d0897f16ec92932
Cristian Alberto Vega Sanhueza <Cristian.Vega@sonda.com>
2024-08-19 23:50:59 -0400

    2024.08.19 cvegasan Se agrega preventDefault

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,6 +5,9 @@ import FormatoMiles from './FormatoMiles';
 import pizzaCart from '../../pizzas.js';
 
 const Cart = () => {
+  const enviarFormulario = (e) => {
+    e.preventDefault()
+  }
     const [cart, setCart] = useState({});
 
     const agregarPizza = (productId, cantidad) => {
@@ -49,36 +52,40 @@ const Cart = () => {
     const totalGeneral=calculateTotal();
 
     return (
-      <div className="cls-carrito">
-          <div className="cls-columna-pizzas">
-            <h3>Carta Pizzas</h3>
-            {pizzaCart.map(product => (
-              <Product
-                key={product.id}
-                product={product}
-                quantity={cart[product.id]?.cantidad || 0}
-                onAdd={agregarPizza}
-                onRemove={eliminarPizza}
-              />
-            ))}
-          </div>
-  
-          <div className="cls-columna-detalle">
-            <h3>Detalle de la compra</h3>
-            {Object.values(cart).length === 0 ? (
-              <p>El carrito está vacío</p>
-            ) : (
-              Object.values(cart).map(item => (
-                                                <div key={item.id} className="cls-item-detalle">
-                                                  <p>Pizza {item.name}</p>
-                                                  <p>Cantidad: {item.cantidad}</p>
-                                                  <p>Total: $<FormatoMiles numero={item.price * item.cantidad}/></p>
-                                                </div>
-              ))
-            )}
-            <h4>Total General: $<FormatoMiles numero={totalGeneral}/></h4>
-            <Button variant="primary">Pagar</Button>
-          </div>
+      <div>
+        <form onSubmit={enviarFormulario}>
+            <div className="cls-carrito">
+                <div className="cls-columna-pizzas">
+                  <h3>Carta Pizzas</h3>
+                  {pizzaCart.map(product => (
+                    <Product
+                      key={product.id}
+                      product={product}
+                      quantity={cart[product.id]?.cantidad || 0}
+                      onAdd={agregarPizza}
+                      onRemove={eliminarPizza}
+                    />
+                  ))}
+                </div>
+        
+                <div className="cls-columna-detalle">
+                  <h3>Detalle de la compra</h3>
+                  {Object.values(cart).length === 0 ? (
+                    <p>El carrito está vacío</p>
+                  ) : (
+                    Object.values(cart).map(item => (
+                                                      <div key={item.id} className="cls-item-detalle">
+                                                        <p>Pizza {item.name}</p>
+                                                        <p>Cantidad: {item.cantidad}</p>
+                                                        <p>Total: $<FormatoMiles numero={item.price * item.cantidad}/></p>
+                                                      </div>
+                    ))
+                  )}
+                  <h4>Total General: $<FormatoMiles numero={totalGeneral}/></h4>
+                  <Button variant="primary">Pagar</Button>
+                </div>
+            </div>
+        </form>
       </div>
     );
   };
